7ca747ba2217bca2d47a6311940b0d11bd1141bb
Rahul Parkar <rahul.a.parkar@gmail.com>
2025-03-08 21:30:52 +0000

    Add Initial xUnit test projet and demo test

---
+++ AirBelgie.Tests/DemoTest.cs
@@ -0,0 +1,19 @@
+﻿namespace AirBelgie.Tests;
+
+public class DemoTest
+{
+    [Fact]
+    public void Demo()
+    {
+        // Arrange
+        int a = 1;
+        int b = 1;
+        int expectedResult = 2;
+
+        // Act
+        int actualResult = a + b;
+        
+        // Assert
+        Assert.Equal(expectedResult, actualResult);
+    }
+}
